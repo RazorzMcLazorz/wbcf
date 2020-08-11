520a185e959bc84bb70641ee9bfbf5d192a07151
RazorzMcLazorz <nateyoungdev@gmail.com>
2020-08-11 13:02:31 -0600

    removing redux stuff

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { Provider } from 'react-redux'
-// import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
@@ -9,30 +7,25 @@ import { MuiTheme } from './theme/MuiTheme'
 
 import App from './pages/app'
 
-// const createStoreWithMiddleware = applyMiddleware()(createStore)
-
 import './style/main.scss'
 
 function main() {
   ReactDOM.render(
-    // <Provider store={createStoreWithMiddleware(reducers)}>
-      <ThemeProvider theme={MuiTheme}>
-        <CssBaseline />
-        <BrowserRouter>
-            {screen.width < 600 ?
-            // Mobile Mode
-            <Switch>
-              <Route exact path="/" component={App}/>
-            </Switch>
-            : // Desktop Mode
-            <Switch>
-              <Route exact path="/" component={App}/>
-            </Switch>
-            }
-          
-        </BrowserRouter>
-      </ThemeProvider>
-    // </Provider>
+    <ThemeProvider theme={MuiTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        {screen.width < 600 ?
+          // Mobile Mode
+          <Switch>
+            <Route exact path="/" component={App} />
+          </Switch>
+          : // Desktop Mode
+          <Switch>
+            <Route exact path="/" component={App} />
+          </Switch>
+        }
+      </BrowserRouter>
+    </ThemeProvider>
     , document.querySelector('.app-wrapper'))
 }
 
