ecbc339e2d336d0006de0a0bb26214b82afeee78
RazorzMcLazorz <nateyoungdev@gmail.com>
2020-08-11 01:18:13 -0600

    tsconfig

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography']
+import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
