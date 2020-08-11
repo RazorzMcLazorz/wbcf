b661f6be6988aa731bc1ab5d348bd79e8b0ae234
RazorzMcLazorz <nateyoungdev@gmail.com>
2020-08-11 01:10:25 -0600

    removed unused import

--- src/theme/MuiTheme.ts
+++ src/theme/MuiTheme.ts
@@ -1,5 +1,4 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-import red from '@material-ui/core/colors/red'
 import grey from '@material-ui/core/colors/grey'
 import purple from '@material-ui/core/colors/purple'
 
